2472fd4ab8e3cf0355b3d51b9c6b92046ee4f2ec
Ambar Khare <ambar.khare24@gmail.com>
2022-02-28 10:13:25 -0700

    update environment

---
+++ lib/environment.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "./trending_stocks/version"
+require_relative "./trending_stocks/cli"
+
+
+module TrendingStocks
+  class Error < StandardError; end
+  # Your code goes here...
+end
