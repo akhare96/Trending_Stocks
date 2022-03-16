21f77544e17025ab8527113acb38da1b0f84655e
Ambar Khare <ambar.khare24@gmail.com>
2022-03-16 11:05:08 -0700

    initialized instance of stocks with two arguements + all array to keep track

--- lib/trending_stocks/stocks.rb
+++ lib/trending_stocks/stocks.rb
@@ -1,3 +1,11 @@
 class TrendingStocks::Stocks
 
+    attr_accessor :ticker, :full_name, :stock_news, :stock_ratings, :stock_details
+
+    @@all = []
+
+    def initialize(ticker, full_name)
+        @ticker = ticker
+        @full_name = full_name
+    end
 end
