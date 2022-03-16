d169ada3ea993ec30ee9df54fe405ac01de6c1e7
Ambar Khare <ambar.khare24@gmail.com>
2022-03-16 07:42:52 -0700

    scraper method for getting stock ticker and name

--- lib/trending_stocks/scraper.rb
+++ lib/trending_stocks/scraper.rb
@@ -1,4 +1,13 @@
 class TrendingStocks::Scraper
-    
+
+    def self.trending_stocks_scraper
+        trending = Nokogiri::HTML(open("https://www.finder.com/reddit-stocks-to-buy"))
+        trending.css("tbody.luna-table__body tr")[0..24].each {|stock|
+            ticker = stock.css("td.text-xx-large").text
+            full_name = stock.css("td.text-x-large").text
+            TrendingStocks::Stocks.new(ticker, full_name)
+        }
+    end
+
 
 end
