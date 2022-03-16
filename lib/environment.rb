eb3f42a1c2f208ab03f9871611cc15be7ce332c1
Ambar Khare <ambar.khare24@gmail.com>
2022-03-16 07:34:33 -0700

    require relative files and require gems

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 require_relative "./trending_stocks/version"
+require_relative "./trending_stocks/scraper"
+require_relative "./trending_stocks/stock_details"
+require_relative "./trending_stocks/stock_ratings"
+require_relative "./trending_stocks/stock_news"
+require_relative "./trending_stocks/stocks"
 require_relative "./trending_stocks/cli"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'terminal-table'
 
 
 module TrendingStocks
