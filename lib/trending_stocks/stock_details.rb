515233c54b3a9417315fa047c28ebb5f63099160
Ambar Khare <ambar.khare24@gmail.com>
2022-03-17 09:25:56 -0700

    initialize method of class details using self/send

--- lib/trending_stocks/stock_details.rb
+++ lib/trending_stocks/stock_details.rb
@@ -1,3 +1,10 @@
 class TrendingStocks::Stock_details
 
+    def initialize(stock_details_hash)
+        stock_details_hash.each {|key, value|
+            self.class.attr_accessor(key)
+            self.send("#{key}=", value)
+        }
+    end
+
 end
