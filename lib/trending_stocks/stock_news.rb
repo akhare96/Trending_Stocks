c88c073de8629a362d4f0bf8f72ac45284862f49
Ambar Khare <ambar.khare24@gmail.com>
2022-03-17 09:13:41 -0700

    added attributes and initialize method for news class

--- lib/trending_stocks/stock_news.rb
+++ lib/trending_stocks/stock_news.rb
@@ -1,3 +1,11 @@
 class TrendingStocks::Stock_news
 
+    attr_accessor :title, :time, :url
+
+    def initialize(title, time, url)
+        @title = title
+        @time = time
+        @url = url
+    end
+
 end
