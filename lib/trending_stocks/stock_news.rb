88fb558a5894ca3adc9e98ef59c6b3bb9266ce0c
Ambar Khare <ambar.khare24@gmail.com>
2022-03-17 09:18:08 -0700

    saves all instances in all_news array

--- lib/trending_stocks/stock_news.rb
+++ lib/trending_stocks/stock_news.rb
@@ -2,10 +2,22 @@ class TrendingStocks::Stock_news
 
     attr_accessor :title, :time, :url
 
+    @@all_news = []
+
     def initialize(title, time, url)
         @title = title
         @time = time
         @url = url
+        save
     end
+
+    def save
+        self.class.all_news << self
+    end
+
+    def self.all_news
+        @@all_news
+    end
+
 
 end
