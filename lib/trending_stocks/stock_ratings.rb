0f167039f848b4cdb451cef39d993fb5ff46384b
Ambar Khare <ambar.khare24@gmail.com>
2022-03-17 10:29:01 -0700

    create method for class instance and set it equal to selected stock attribute

--- lib/trending_stocks/stock_ratings.rb
+++ lib/trending_stocks/stock_ratings.rb
@@ -5,5 +5,10 @@ class TrendingStocks::Stock_ratings
     def initialize(ratings)
         @ratings = ratings
     end
-    
+
+    def create_stock_ratings(selected_stock)
+        analysts = TrendingStocks::Scraper.stock_analyst_scraper(selected_stock)
+        selected_stock.ratings = analysts
+    end
+
 end
