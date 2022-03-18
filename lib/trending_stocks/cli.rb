475569fec20efcf817a3b5ea1929c886096120cb
Ambar Khare <ambar.khare24@gmail.com>
2022-03-18 12:42:10 -0700

    takes user choice and displays ratings, news, or selects another stock

--- lib/trending_stocks/cli.rb
+++ lib/trending_stocks/cli.rb
@@ -1,21 +1,16 @@
 
 class TrendingStocks::CLI
 
-
     def call
-
         @input = ""
-
-        puts "Welcome to Trending Stocks! Here are the top 25 trending stocks on reddit for the past 24 hours.  The current time is #{Time.now}."
+        puts "Welcome to Trending Stocks! Here are the top 25 trending stocks on reddit for the past 24 hours.  The current time is #{Time.now}.  Enter 'exit' anytime during the program to exit."
         TrendingStocks::Stocks.create_stocks_list
         TrendingStocks::Stocks.display_stocks_list
         puts "Which stock would you like to see more details of? Please enter a number from 1-25"
         gets_stock_information
-        puts "\n"
         TrendingStocks::Stock_details.display_stock_details(@selected_stock)
-        binding.pry
-        
-
+        puts "For news, type 'news'.  For analysts ratings type 'ratings'.  If you would like to select another stock type 'next'"
+        user_choice
     end
 
     def gets_stock_information
@@ -27,50 +22,41 @@ class TrendingStocks::CLI
             TrendingStocks::Stock_details.create_stock_details(@selected_stock)
             TrendingStocks::Stock_ratings.create_stock_ratings(@selected_stock)
             TrendingStocks::Stock_news.create_stock_news(@selected_stock)
-        elsif @input == "exit"
-            puts "Thanks for using Trending Stocks! Goodbye."
-        else
+        elsif
             puts "Please enter a valid number"
             gets_stock_information
+        else
+            goodbye
         end
     end
 
-/
-    welcomes user
+    def user_choice
+        @input = gets.strip
+        if @input == "news"
+            TrendingStocks::Stock_news.display_stock_news(@selected_stock)
+            puts "If you would like to see ratings type 'ratings' or 'next' to select another stock."
+            user_chose_news_first
+        elsif @input == "ratings"
+            TrendingStocks::Stock_ratings.display_stock_ratings(@selected_stock)
+            puts "If you would like to see latest news type 'news' or 'next' to select another stock."
+            user_chose_ratings_first
+        elsif @input == "next"
+            user_chose_next
+        else
+            goodbye
+        end
+    end
 
-scrapes stocks list
+    def user_chose_news_first
+    end
 
-displays stock list
+    def user_chose_ratings_first
+    end
 
-asks user to select which stock would like to see details of
+    def user_chose_next
+    end
 
-user selects stock
+    def goodbye
+    end
 
-scrapes details for selected stock
-
-scrapes stock news for stock
-
-scrapes stock analyst ratings
-
-displays details of stock
-
-asks user to display news, ratings, or start over
-
-user selects
-
-if news then displays news
-
-    asks for ratings or start over
-
-        start over or exit
-
-if ratings then displays ratings
-
-    asks for news or start over
-
-        start over or exit
-
-if start over then clear data and start over
-    /
 end
-
