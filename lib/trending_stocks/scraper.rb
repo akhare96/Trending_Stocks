817c1649276044bb0950b56760452005ac388142
Ambar Khare <ambar.khare24@gmail.com>
2022-03-22 21:43:28 -0700

    final update

--- lib/trending_stocks/scraper.rb
+++ lib/trending_stocks/scraper.rb
@@ -2,7 +2,6 @@ require 'pry'
 class TrendingStocks::Scraper
 
     def self.trending_stocks_scraper
-        
         trending = Nokogiri::HTML(open("https://www.finder.com/reddit-stocks-to-buy"))
         trending.css("tbody.luna-table__body tr")[0..24].each {|stock|
             ticker = stock.css("td.text-xx-large").text
