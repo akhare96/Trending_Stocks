2317404ad067a54dbfd7b37028144dcf419c27e8
Ambar Khare <ambar.khare24@gmail.com>
2022-04-05 22:47:07 -0700

    created ratings array and methods to access it

--- lib/trending_stocks/stock_ratings.rb
+++ lib/trending_stocks/stock_ratings.rb
@@ -2,13 +2,23 @@ class TrendingStocks::Stock_ratings
     
     attr_accessor :rates
 
+    @@ratings = []
+
     def initialize(rates)
         @rates = rates
     end
 
+    def save
+        self.class.ratings << self
+    end
+
+    def self.ratings
+        @@ratings
+    end
+
     def self.create_stock_ratings(selected_stock)
-        analysts = TrendingStocks::Scraper.stock_analyst_scraper(selected_stock)
-        selected_stock.ratings = analysts
+        TrendingStocks::Scraper.stock_analyst_scraper(selected_stock)
+        selected_stock.ratings = self.ratings[0]
     end
 
     def self.display_stock_ratings(user_stock)
